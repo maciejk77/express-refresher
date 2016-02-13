7c623acaaf362ddae224549a446c6bde5a47564b
Maciej Kurek <maciejk77@gmail.com>
2016-02-13 19:54:29 +0000

    New route for html

--- intro/app.js
+++ intro/app.js
@@ -9,7 +9,13 @@ app.get('/', function(request, response) {
 
 app.get('/blocks', function(request, response) {
   var blocks = ['Fixed', 'Movable', 'Rotating'];
-  response.send(blocks);
+  // response.send(blocks); // .send automatically serialize to JSON as a default
+  response.json(blocks); // same as above, serialize to JSON
+});
+
+app.get('/sometext', function(request, response) {
+  var sometext = '<ul><li>Some</li><li>Text</li></ul>';
+  response.send(sometext);
 });
 
 app.listen(3000, function() {
